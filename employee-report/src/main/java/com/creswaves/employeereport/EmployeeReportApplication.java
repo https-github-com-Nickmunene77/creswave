e2732ec5a2ef0162aa857c521fd518d57ac8af07
Nickson Munene Njeru <nicknionene7@gmail.com>
2024-05-14 16:46:38 +0300

    Added jasper report header and attached a spring boot application

---
+++ employee-report/src/main/java/com/creswaves/employeereport/EmployeeReportApplication.java
@@ -0,0 +1,13 @@
+package com.creswaves.employeereport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeReportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeReportApplication.class, args);
+	}
+
+}
---
+++ employee-report/src/test/java/com/creswaves/employeereport/EmployeeReportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.creswaves.employeereport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeReportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
